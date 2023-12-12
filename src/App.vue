7bec0d80f4b65b24bc75388228303df7872d3827
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-12-12 15:23:57 +0330

    Build the Header component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <script setup>
+import Header from "./components/Header.vue";
 </script>
 
 <template>
-  <header>
-   
-  </header>
-
-  <main>
-  </main>
+  <Header />
+  <main></main>
 </template>
 
 <style scoped></style>
---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <h1>todo</h1>
+    <!-- TODO add icon -->
+    <img src="" alt="" />
+  </header>
+</template>
+
+<style scoped>
+h1 {
+  text-transform: capitalize;
+  letter-spacing: 8px;
+}
+</style>
