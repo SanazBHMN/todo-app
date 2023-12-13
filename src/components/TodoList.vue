5b53b1bcb013bc1d3d54fd9a2874101bd612c901
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-12-13 14:48:10 +0330

    Renam Todo to TodoItem

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,13 +1,13 @@
 <script setup>
 import { defineProps, defineEmits } from "vue";
-import Todo from "../components/Todo.vue";
+import TodoItem from "../components/TodoItem.vue";
 
 const { todos } = defineProps(["todos"]);
 const emit = defineEmits(["remove-todo"]);
 </script>
 
 <template>
-  <Todo
+  <TodoItem
     v-for="todo in todos"
     :key="todo.id"
     :id="todo.id"
