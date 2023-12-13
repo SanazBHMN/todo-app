268fbaa08acbee0868eb3932fe4d1136548ece7b
Sanaz Bahmani <sanazbahmani35@gmail.com>
2023-12-13 15:20:01 +0330

    Implement functions to show all, acitve and completed todos

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, computed } from "vue";
+import { ref, computed, reactive } from "vue";
 import Header from "./components/Header.vue";
 import TodoInput from "./components/TodoInput.vue";
 import TodoList from "./components/TodoList.vue";
@@ -7,6 +7,9 @@ import TodoList from "./components/TodoList.vue";
 import d from "./data/data.json";
 
 const todos = ref(d);
+const displayedTodos = reactive({
+  items: [...todos.value],
+});
 
 const handleAddTodo = (newTodoText) => {
   const newTodo = {
@@ -21,6 +24,18 @@ const handleRemoveTodo = (todoId) => {
   todos.value = todos.value.filter((todo) => todo.id !== todoId);
 };
 
+const showAllTodos = () => {
+  displayedTodos.items = [...todos.value];
+};
+
+const showActiveTodos = () => {
+  displayedTodos.items = todos.value.filter((todo) => todo.isActive);
+};
+
+const showCompletedTodos = () => {
+  displayedTodos.items = todos.value.filter((todo) => todo.completed);
+};
+
 const activeTodos = computed(() => todos.value.filter((todo) => todo.isActive));
 </script>
 
@@ -28,9 +43,12 @@ const activeTodos = computed(() => todos.value.filter((todo) => todo.isActive));
   <main class="container">
     <Header />
     <TodoInput @add-todo="handleAddTodo" />
-    <TodoList :todos="todos" @remove-todo="handleRemoveTodo" />
+    <TodoList :todos="displayedTodos.items" @remove-todo="handleRemoveTodo" />
     <div>
       <small>{{ activeTodos.length }} items left</small>
+      <button @click="showAllTodos">All</button>
+      <button @click="showActiveTodos">Active</button>
+      <button @click="showCompletedTodos">Completed</button>
     </div>
   </main>
 </template>
